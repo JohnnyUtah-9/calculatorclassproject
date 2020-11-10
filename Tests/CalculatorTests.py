ae9d46abe100fe35c4359fa844a38aa98aedd2ce
JohnnyUtah-9 <sidney.b.johnson3@gmail.com>
2020-11-10 13:02:06 -0500

    Task: Uploaded sample csv datafiles for unittesting.

--- Tests/CalculatorTests.py
+++ Tests/CalculatorTests.py
@@ -4,7 +4,7 @@ from CsvReader.CsvReader import CsvReader, ClassFactory
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.csv_reader = CsvReader('Tests/Data/UnitTestAddition.csv')
+        self.csv_reader = CsvReader('Tests/Data/Addition.csv')
 
     def test_return_data_as_objects(self):
         people = self.csv_reader.return_data_as_objects('Value 1')
