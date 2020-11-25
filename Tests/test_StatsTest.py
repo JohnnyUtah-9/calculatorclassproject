4d80babc9bc1d4cca08f351d44d40dafa2539f1d
JohnnyUtah-9 <sidney.b.johnson3@gmail.com>
2020-11-24 20:17:33 -0500

    Bug: updated absolute path configuration file.

--- Tests/test_StatsTest.py
+++ Tests/test_StatsTest.py
@@ -1,3 +1,4 @@
+"""Test"""
 import unittest
 from CsvReader.CsvReader import CsvReader
 from Stats.Statistics import Statistics
@@ -18,6 +19,7 @@ class MyTestCase(unittest.TestCase):
     sample_answer = CsvReader('Tests/Data/TestStatAnswers.csv').data
 
     def setUp(self) -> None:
+        """Test"""
         self.statistics = Statistics()
 
     def test_instantiate_calculator(self):
@@ -29,45 +31,46 @@ class MyTestCase(unittest.TestCase):
     def test_population_mean_statistics(self):
         for row in self.test_answer:
             pprint(row["mean"])
-        self.assertEqual(self.statistics.population_mean(self.column1), float(row['mean']))
-        self.assertEqual(self.statistics.result, float(row['mean']))
+        self.assertEqual(self.statistics.population_mean(self.column1), float(self.row['mean']))
+        self.assertEqual(self.statistics.result, float(self.row['mean']))
 
     def test_median_statistics(self):
         for row in self.test_answer:
             pprint(row["median"])
-        self.assertEqual(self.statistics.median(self.column1), float(row['median']))
-        self.assertEqual(self.statistics.result, float(row['median']))
+        self.assertEqual(self.statistics.median(self.column1), float(self.row['median']))
+        self.assertEqual(self.statistics.result, float(self.row['median']))
 
     def test_mode_statistics(self):
         for row in self.test_answer:
             pprint(row["mode"])
-        self.assertEqual(self.statistics.mode(self.column1), float(row['mode']))
-        self.assertEqual(self.statistics.result, float(row['mode']))
+        self.assertEqual(self.statistics.mode(self.column1), float(self.row['mode']))
+        self.assertEqual(self.statistics.result, float(self.row['mode']))
 
     def test_standard_deviation_statistics(self):
         for row in self.test_answer:
             pprint(row["stddev"])
-        self.assertEqual(self.statistics.stddev(self.column1), float(row['stddev']))
-        self.assertEqual(self.statistics.result, float(row['stddev']))
+        self.assertEqual(self.statistics.stddev(self.column1), float(self.row['stddev']))
+        self.assertEqual(self.statistics.result, float(self.row['stddev']))
 
     def test_proportion_variance_statistics(self):
         for row in self.test_answer:
             pprint(row['proportion_variance'])
-        self.assertEqual(self.statistics.population_proportion_variance(self.column1), float(row['proportion_variance']))
-        self.assertEqual(self.statistics.result, float(row['proportion_variance']))
+        self.assertEqual(self.statistics.population_proportion_variance(self.column1),
+                         float(self.row['proportion_variance']))
+        self.assertEqual(self.statistics.result, float(self.row['proportion_variance']))
 
     def test_variance_statistics(self):
         for row in self.test_answer:
             pprint(row['variance'])
-        self.assertEqual(self.statistics.variance(self.column1), float(row['variance']))
-        self.assertEqual(self.statistics.result, float(row['variance']))
+        self.assertEqual(self.statistics.variance(self.column1), float(self.row['variance']))
+        self.assertEqual(self.statistics.result, float(self.row['variance']))
 
     def test_correlation_statistics(self):
         for row in self.test_answer:
             pprint(row['correlation'])
         self.assertEqual(self.statistics.correlation_coefficient(self.column1, self.column2),
-                         float(row['correlation']))
-        self.assertEqual(self.statistics.result, float(row['correlation']))
+                         float(self.row['correlation']))
+        self.assertEqual(self.statistics.result, float(self.row['correlation']))
 
     def test_zscore_statistics(self):
         self.assertEqual(self.statistics.zscore(self.column1), self.column_zscore)
@@ -85,26 +88,26 @@ class MyTestCase(unittest.TestCase):
         for row in self.test_answer:
             pprint(row['ci_top'])
             pprint(row['ci_bottom'])
-        self.assertEqual(self.statistics.confidence_interval_top(self.column1), float(row['ci_top']))
-        self.assertEqual(self.statistics.confidence_interval_bottom(self.column1), float(row['ci_bottom']))
+        self.assertEqual(self.statistics.confidence_interval_top(self.column1), float(self.row['ci_top']))
+        self.assertEqual(self.statistics.confidence_interval_bottom(self.column1), float(self.row['ci_bottom']))
 
     def test_sample_mean_statistics(self):
         for row in self.sample_answer:
             pprint(row["mean"])
-        self.assertEqual(self.statistics.sample_mean(self.column3), float(row['mean']))
-        self.assertEqual(self.statistics.result, float(row['mean']))
+        self.assertEqual(self.statistics.sample_mean(self.column3), float(self.row['mean']))
+        self.assertEqual(self.statistics.result, float(self.row['mean']))
 
     def test_sample_standard_deviation_statistics(self):
         for row in self.sample_answer:
             pprint(row["stddev"])
-        self.assertEqual(self.statistics.sample_stddev(self.column3), float(row['stddev']))
-        self.assertEqual(self.statistics.result, float(row['stddev']))
+        self.assertEqual(self.statistics.sample_stddev(self.column3), float(self.row['stddev']))
+        self.assertEqual(self.statistics.result, float(self.row['stddev']))
 
     def test_sample_variance_statistics(self):
         for row in self.sample_answer:
             pprint(row['variance'])
-        self.assertEqual(self.statistics.sample_variance(self.column3), float(row['variance']))
-        self.assertEqual(self.statistics.result, float(row['variance']))
+        self.assertEqual(self.statistics.sample_variance(self.column3), float(self.row['variance']))
+        self.assertEqual(self.statistics.result, float(self.row['variance']))
 
 
 if __name__ == '__main__':
